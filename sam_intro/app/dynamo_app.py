a8f6e0b08f2fc4d76b3d969390cbd99f29093e5b
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-08-28 21:56:48 +0900

    ✨  add dynamo app

---
+++ sam_intro/app/dynamo_app.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+import os
+import decimal
+from boto3.dynamodb.conditions import Key
+
+
+DYNAMODB_ENDPOINT = os.environ.get(
+    "DYNAMODB_ENDPOINT", "http://localhost:8000")
+
+dynamodb = boto3.resource(
+    'dynamodb', endpoint_url=DYNAMODB_ENDPOINT)
+
+table = dynamodb.Table('athlete')
+
+
+def type_handler(x):
+    return float(x) if isinstance(x, decimal.Decimal) else None
+
+
+def handler(event, context):
+    path_parameters = event.get('pathParameters')
+    if path_parameters is None:
+        return {
+            'statusCode': 200,
+            'body':
+                json.dumps(table.scan(Limit=10), default=type_handler)
+        }
+    else:
+        id = int(path_parameters.get('id'))
+        return {
+            'statusCode': 200,
+            'body':
+                json.dumps(table.query(
+                    KeyConditionExpression=Key('ID').eq(id)
+                ), default=type_handler)
+        }
